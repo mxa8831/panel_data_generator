21f989b2ccc3dff3518e62b46bcf30710ce47c54
rahmatnazali95 <rahmatnazali95@gmail.com>
2019-08-28 16:08:07 +0700

    add exception for missing sheet

--- function_file.py
+++ function_file.py
@@ -1,12 +1,20 @@
 import pandas
+from xlrd.biffh import XLRDError
 
 def open_excel(fileName, sheetName):
-    dataFrame = pandas.read_excel(fileName, sheet_name=sheetName)
-    return dataFrame
+    try:
+        dataFrame = pandas.read_excel(fileName, sheet_name=sheetName)
+        return dataFrame
+    except XLRDError as e:
+        print(e)
+        return None
+
 
 def open_input_file(filename, sheetName):
     df = open_excel(filename,sheetName)
-    return df.loc[4:]
+    if df is not None:
+        return df.loc[4:]
+    return None
 
 def open_output_file(filename, sheetName):
     return open_excel(filename,sheetName)
