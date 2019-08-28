6669accf029ed0e2832961597c1e682e58844f7f
rahmatnazali95 <rahmatnazali95@gmail.com>
2019-08-28 16:37:52 +0700

    get title and unit

--- function_file.py
+++ function_file.py
@@ -11,10 +11,17 @@ def open_excel(fileName, sheetName):
 
 
 def open_input_file(filename, sheetName):
-    df = open_excel(filename,sheetName)
-    if df is not None:
-        return df.loc[4:]
-    return None
+    dataframe = open_excel(filename,sheetName)
+    if dataframe is not None:
+        title = dataframe.columns[0]
+        unit = get_unit(dataframe)
+        return title, unit, dataframe.loc[4:]
+    return (None, None, None)
 
 def open_output_file(filename, sheetName):
     return open_excel(filename,sheetName)
+
+def get_unit(datafarme):
+    # （単位：件）
+    aRow = datafarme.loc[0, :].tolist()
+    return aRow[-1]
