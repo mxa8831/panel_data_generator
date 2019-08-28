f6bf92e05bdd6c6cfa2772aca9b8bfad7eadcad6
rahmatnazali95 <rahmatnazali95@gmail.com>
2019-08-28 13:26:43 +0700

    glob for file list

--- function_general.py
+++ function_general.py
@@ -1,7 +1,11 @@
 import re
+import glob
 
 def re_get_date(filename):
     result = re.search("A_pref(20\d\d)(\d\d)", filename)
     if result and len(result.groups()) == 2:
         return (result.group((1)), result.group((2)))
     raise ValueError("The file name did not contain the pattern of year and month as YYYYMM. Example of working pattern: '201903'.")
+
+def list_file(pathname):
+    return glob.glob("{}/*.xls*".format(pathname))
