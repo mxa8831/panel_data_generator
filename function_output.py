5f2ff649775eed10febf1bd8a6c948bfbd99aca4
rahmatnazali95 <rahmatnazali95@gmail.com>
2019-08-29 17:58:51 +0700

    func for generating output

--- function_output.py
+++ function_output.py
@@ -1,4 +1,6 @@
 import function_general
+import datetime
+import pandas
 
 summaryHeaderNameList = [
     'Year',
@@ -34,3 +36,10 @@ def get_net_header_name():
 
 def is_output_file_available(pathName):
     return len(function_general.list_file(pathName)) != 0
+
+def generate_output(summaryDf, netDf, filename = "output_file/beta_{}.xlsx".format(str(datetime.datetime.now()).replace(":", "."))):
+    excelWriter = pandas.ExcelWriter(filename, engine='xlsxwriter')
+    summaryDf.to_excel(excelWriter, index=False, sheet_name="Summary1")
+    netDf.to_excel(excelWriter, index=False, sheet_name="net")
+    excelWriter.save()
+
