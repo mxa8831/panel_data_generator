63907a3a4c71e2e3914412e38690727f7968e590
rahmatnazali95 <rahmatnazali95@gmail.com>
2019-08-29 11:35:51 +0700

    test script

--- tester_script.py
+++ tester_script.py
@@ -7,7 +7,7 @@ import pandas
 
 for aFile in function_general.list_file("input_file"):
     print("Processing {}".format(aFile))
-    year, month = function_general.re_get_date(aFile)
+    year, month = function_general.regex_get_date(aFile)
 
     title, unit, df1 = function_file.open_input_file(aFile, function_input.get_sheet_name(0))
     print(title, unit)
@@ -18,14 +18,14 @@ exit()
 # test file reader
 fileList = function_general.list_file("input_file")
 for aFile in fileList:
-    print(aFile, function_general.re_get_date(aFile))
+    print(aFile, function_general.regex_get_date(aFile))
     break
 
 # test input file checker
 print(function_input.is_output_file_available("output_file"))
 
 # opening input file
-year, month = function_general.re_get_date("input_file/A_pref201903.xlsx")
+year, month = function_general.regex_get_date("input_file/A_pref201903.xlsx")
 print(year, month)
 df = function_file.open_input_file("input_file/A_pref201903.xlsx", function_input.get_sheet_name(0))
 rowlist = function_input.process_dataframe(df, year, month)
