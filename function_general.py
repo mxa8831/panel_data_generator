476087a11c84abfe9a774f673aa3a77c913a6f79
rahmatnazali95 <rahmatnazali95@gmail.com>
2019-09-04 14:01:38 +0700

    func output reader

--- function_general.py
+++ function_general.py
@@ -7,8 +7,8 @@ def regex_get_date(filename):
         return (result.group((1)), result.group((2)))
     raise ValueError("The file name did not contain the pattern of year and month as YYYYMM. Example of working pattern: '201903'.")
 
-def list_file(pathname):
-    return sorted(glob.glob("{}/*.xls*".format(pathname)))
+def list_file(pathname, suffix = '*'):
+    return sorted(glob.glob("{}/{}.xls*".format(pathname, suffix)))
 
 def string_to_int(string):
     return int(string)
--- function_output.py
+++ function_output.py
@@ -35,8 +35,14 @@ def get_summary_header_name():
 def get_net_header_name():
     return netHeaderNameList
 
-def is_output_file_available(pathName):
-    return len(function_general.list_file(pathName)) != 0
+def is_output_file_available(pathName, suffix = '*'):
+    return len(function_general.list_file(pathName, suffix)) != 0
+
+def get_output_file(pathName, suffix='*'):
+    list_file = function_general.list_file(pathName, suffix)
+    if list_file:
+        return list_file[-1]
+    return None
 
 def generate_output(summaryDf, netDf, filename = "output_file/beta_{}.xlsx".format(str(datetime.datetime.now()).replace(":", "."))):
     excelWriter = pandas.ExcelWriter(filename, engine='xlsxwriter')
